2b614992cec7f27d3e7425dce65d2139463e4ac7
Bundyo <bundyo@gmail.com>
2019-11-27 08:53:27 +0200

    Tests

--- app/main-page.js
+++ app/main-page.js
@@ -11,24 +11,7 @@ JavaScript modules defined in other files.
 */
 const createViewModel = require("./main-view-model").createViewModel;
 
-function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    const page = args.object;
-
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
+function onNavigatingTo({ object: page }) {
     page.bindingContext = createViewModel();
 }
 
